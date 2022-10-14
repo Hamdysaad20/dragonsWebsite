c81331773a085f4de2a6eb7e494af4ec855cb63f
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-10-14 16:11:59 +0200

    toggle error

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,10 +18,10 @@ function Header(props) {
   const toggler = () => {
     setBurgerMenu(!burgerMenu);
     var x = document.getElementById("togglerDiv");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (x.style.display === "block") {
+      x.style.display = "none";
     } else {
-      x.style.display = "none ";
+      x.style.display = "block";
     }
   };
   return (
