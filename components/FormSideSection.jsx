8447bb80499937e2f60173f7f8ecd91b432f80e0
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-10-28 15:46:30 +0200

    f

--- components/FormSideSection.jsx
+++ components/FormSideSection.jsx
@@ -6,9 +6,9 @@ function FormSideSection() {
   return (
     <div className='lg:w-[500px] relative  h-full flex justify-center mx-auto min-w-[300px]'>
       <div className='w-[200px] hover:scale-90 duration-1000 lg:mb-[100px] lg:w-[500px]'>
-        <div className='w-full flex justify-center mx-auto text-xl font-bold  text-green-400 bg-green-100 py-3 rounded-xl'>
+        {/* <div className='w-full flex justify-center mx-auto text-xl font-bold  text-green-400 bg-green-100 py-3 rounded-xl'>
           (HR) is Done{"😄"}
-        </div>
+        </div> */}
         <Lottie animationData={hand} />{" "}
         <div>
           <h1
