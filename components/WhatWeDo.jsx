3c5d72bf846435aeaf9a95d00e1c718ddebc43f0
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-11-15 04:42:06 +0200

    d

--- components/WhatWeDo.jsx
+++ components/WhatWeDo.jsx
@@ -32,11 +32,12 @@ function WhatWeDo(props) {
       <div className='pt-32  hover:scale-95 duration-1000  md:pt-24 pl-10 text-white relative hidden lg:block'>
         <Image
           className='relative  bounce-2 box2 '
-          src={"/Images/eventPhoto1.png"}
+          src={"https://i.ibb.co/fM8Ynqj/event-Photo1.png"}
           alt='Event Image'
           height='530'
           width='730'     
-        
+          placeholder='blur'
+          blurDataURL={`/_next/image?url=${props.sec2Image}&w=16&q=1`}
         />
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i.ibb.co'],
+  },
 }
 
 module.exports = nextConfig
+
+// 
