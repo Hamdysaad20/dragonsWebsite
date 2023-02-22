13343c61c0053ee0a41a76e6cd74cf5957f9a9e1
YosefJoo <yosef2020go@gmail.com>
2023-02-22 14:58:48 +0200

    handle _app to comply with the changes 🤩 "

--- pages/_app.js
+++ pages/_app.js
@@ -5,23 +5,23 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-          <Head>
-        <title>Dragons</title>
-        <meta
-          property='og:Dragons'
-          content='Dragons'
-          key='Dragons'
-        />       
-        <link rel='shortcut icon' href='/images/dragonsEG.png' />
-      </Head>
+        <Head>
+            <title>Dragons</title>
+                <meta
+                  property='og:Dragons'
+                  content='Dragons'
+                  key='Dragons'
+                />
+            <link rel='shortcut icon' href='/images/dragonsEG.png' />
+        </Head>
       <Header
         className='select-none'
         herfFacebook='https://www.facebook.com/profile.php?id=100086806926973'
         herfGithub='https://github.com/Dragonsfu'
-        HerfDash='/Join'
+        HerfDash='/Join/Join'
         Herfabout='/about'
-        HerfActivity='/Activities'
-        HerfService='/Team'
+        HerfActivity='/Activities/Activities'
+        HerfService='/Team/Team'
         herflogoLink="/"
         herflogo='/images/dragonsEG.png'
         herflogo2='/images/logotext.png'
