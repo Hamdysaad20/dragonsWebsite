d689d9a0b9608941b72fe447319e6d3880dbaa9a
hamdysaad20 <hamdysaad.biz@gmail.com>
2023-03-07 14:37:57 +0200

    No SSR in Profile

--- pages/users/profile.jsx
+++ pages/users/profile.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from './../../components/profile/Header';
+import NoSSRCompParent from '../../components/NoSSRCompParent';
+
 // info area
 // user act
 // 
@@ -10,7 +12,10 @@ function profile() {
   <div className="pt-20 pb-40 max-w-[1400px] h-full flex mx-auto justify-center ">
     
  <div className="w-full h-full">
+  <NoSSRCompParent>
+    
  <Header/>
+ </NoSSRCompParent>
  </div>
   </div>
     )
